121df35315d6edaa543efedd28d63483f765d68b
tategakibunko <lambda.watanabe@gmail.com>
2020-08-11 21:49:02 +0900

    Exported other modules

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,3 +1,6 @@
 export * from './types';
 export * from './effect';
+export * from './effect-env';
+export * from './menu';
+export * from './plugins';
 export { ComboPlayer } from './combo-player';
